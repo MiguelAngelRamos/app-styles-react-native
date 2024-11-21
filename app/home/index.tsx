a1820607e0a24237260f62773eda0e2688e5123a
Miguel Ramos <return.infox@gmail.com>
2024-11-21 15:34:19 -0300

    props del boton

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,16 +2,19 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ButtonCustom from '@/components/shared/ButtonCustom'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
       <View className="px-10 mt-5">
         <Link href="/profile">Profile</Link>
-        <ButtonCustom/>
-        <ButtonCustom/>
-        <ButtonCustom/>
+        <ButtonCustom
+          onPress={() => router.push('/characters')}
+          color="secondary"
+        >Characteres
+        </ButtonCustom>
+     
       </View>
     </SafeAreaView>
 
--- components/shared/ButtonCustom.tsx
+++ components/shared/ButtonCustom.tsx
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, PressableProps } from 'react-native'
 import React from 'react'
 
-const ButtonCustom = () => {
+interface Props extends PressableProps {
+  children: string;
+  color?: 'primary'|'secondary'|'tertiary';
+  className?: string
+}
+const ButtonCustom = ({children, color ='primary', onPress, className, onLongPress}:Props) => {
+
+ const btnColor = {
+  primary: 'bg-primary',
+  secondary: 'bg-secondary',
+  tertiary: 'bg-tertiary'
+ }[color];
+
+ const textColor = {
+  primary: 'text-primary',
+  secondary: 'text-secondary',
+  tertiary: 'text-tertiary'
+ }[color];
+
   return (
-    <View>
-      <Text>ButtonCustom</Text>
-    </View>
+   <Pressable
+    className={`p-3 ${btnColor} ${className}`}
+   onPress={onPress}
+   >
+    <Text className={`text-center ${textColor}`}>{children}</Text>
+   </Pressable>
   )
 }
 
