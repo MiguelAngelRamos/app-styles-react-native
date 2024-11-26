673ba77b5bb39f858cd3c01f5d017564c511d340
Miguel Ramos <return.infox@gmail.com>
2024-11-26 17:03:50 -0300

    navegacion parte 2

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,33 +1,48 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { Drawer } from 'expo-router/drawer';
+import {Ionicons} from '@expo/vector-icons';
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
-    <Drawer.Screen
-        name="tabs" // This is the name of the page and must match the url from root
-        options={{
-          drawerLabel: 'Tabs',
-          title: 'Tabs-Stacks',
-        }}
-      />
+    <Drawer
+      screenOptions={{
+        headerShown: true,
+        overlayColor: 'rgba(0,0,0,0.4)',
+        drawerActiveTintColor: 'indigo',
+        headerShadowVisible: false,
+        sceneStyle: {
+          backgroundColor: 'white'
+        }
+      }}
+    >
       <Drawer.Screen
-        name="user/index" // This is the name of the page and must match the url from root
-        options={{
-          drawerLabel: 'User',
-          title: 'User Screen',
-        }}
-      />
-      <Drawer.Screen
-        name="settings/index" // This is the name of the page and must match the url from root
-        options={{
-          drawerLabel: 'Settings',
-          title: 'Settings Screen',
-        }}
-      />
+          name="tabs" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Tabs',
+            title: 'Tabs-Stacks',
+            drawerIcon: ({color, size}) => (<Ionicons name="apps" size={size} color={color}></Ionicons>)
+          }}
+        />
+        <Drawer.Screen
+          name="user/index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'User',
+            title: 'User Screen',
+            drawerIcon: ({color, size}) => (<Ionicons name="person-circle-outline" size={size} color={color}></Ionicons>)
+          }}
+        />
+        <Drawer.Screen
+          name="settings/index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Settings',
+            title: 'Settings Screen',
+            drawerIcon: ({color, size}) => (<Ionicons name="compass" size={size} color={color}></Ionicons>)
+          }}
+        />
   </Drawer>
   )
 }
 
 export default DrawerLayout;
+
